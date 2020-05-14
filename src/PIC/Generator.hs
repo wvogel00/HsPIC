7b42f4f465039af85911da8d8811e773cc7c04a8
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-05-14 11:45:11 +0900

    リトルエンディアン変換追加

--- src/PIC/Generator.hs
+++ src/PIC/Generator.hs
@@ -18,7 +18,7 @@ testRecord = Record {
 data RecType = Data
             | FileFin
             | ExSegmentAddr
-            | StartSeqmentAddr
+            | StartSegmentAddr
             | ExLinearAddr
             | StartLinearAddr deriving (Eq, Show)
 
@@ -31,12 +31,12 @@ data Record = Record {
 } deriving (Eq, Show)
 
 rectypeBits :: RecType -> B.ByteString
-rectypeBits Data = B.singleton 0
-rectypeBits FileFin = B.singleton 1
-recTypeBits ExSegmentAddr = B.singleton 3
-rectypeBits StartSeqmentAddr = B.singleton 4
-rectypeBits ExLinearAddr = B.singleton 5
-rectypeBits StartLinearAddr = B.singleton 6
+rectypeBits Data            = B.singleton 0
+rectypeBits FileFin         = B.singleton 1
+rectypeBits ExSegmentAddr   = B.singleton 2
+rectypeBits StartSegmentAddr= B.singleton 3
+rectypeBits ExLinearAddr    = B.singleton 4
+rectypeBits StartLinearAddr = B.singleton 5
 
 insertChkSum :: Record -> B.ByteString
 insertChkSum rec = B.singleton.complement2.B.foldl1 (+)
@@ -46,12 +46,20 @@ insertChkSum rec = B.singleton.complement2.B.foldl1 (+)
 add w1 w2 = w8 $ mod (word8ToInt w1 + word8ToInt w2) 255
 complement2 = add (w8 1).complement
 
+--リトルエンディアンに変換
+--littleEndian :: B.ByteString -> B.ByteString
+littleEndian = B.pack.convert.B.unpack where
+    convert [] = []
+    convert [a] = [w8 0, a]
+    convert (w1:w2:bs) = w2:w1:convert bs
+
 outputHexFile :: FilePath -> [Record] -> IO ()
 outputHexFile file = B.writeFile file.BC.unlines.map outputRecFormat
 
 outputRecFormat :: Record -> B.ByteString
-outputRecFormat rec = BC.pack ":" `B.append` (B.singleton.w8 $ len rec) `B.append` (offset rec)
-        `B.append` (rectypeBits $ rectype rec) `B.append` (_data rec) `B.append` (chksum rec)
+outputRecFormat rec = B.append (BC.pack ":") $
+        (B.singleton.w8 $ len rec) `B.append` (offset rec) `B.append`
+        (rectypeBits $ rectype rec) `B.append` (littleEndian $ _data rec) `B.append` (chksum rec)
 
 
 
