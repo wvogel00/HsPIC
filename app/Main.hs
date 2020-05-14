cca457bf403059ebfce25aab5daba98dcd4f1539
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-05-14 12:59:13 +0900

    メイン関数完成

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,23 @@ module Main where
 
 import Lib
 import PIC.Generator
+import PIC.Parser
+import System.Environment (getArgs)
+import Text.Trifecta.Result
+
+fromSuccess v = case v of
+    Success a -> a
+    Failure _ -> []
 
 main :: IO ()
-main = someFunc
+main = do
+    (filename : _) <- getArgs
+    if filename == []
+        then putStrLn "プロジェクトファイルを指定してください"
+        else do
+            result <- runParse <$> readFile filename
+            case result of
+                Success contents -> do
+                    outputHexFile "test.hex" $ genRecords contents
+                    putStrLn "正常にhexファイルを生成しました"
+                Failure _ -> putStrLn "ファイルエラーです"
