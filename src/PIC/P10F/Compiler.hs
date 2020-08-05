70080902a0db4947706560677a737de247bcb641
wvogel00 <eagle.hokkaido2317@gmail.com>
2020-08-05 14:09:56 +0900

    数字パーサを実装

--- src/PIC/P10F/Compiler.hs
+++ src/PIC/P10F/Compiler.hs
@@ -5,8 +5,12 @@ import Text.Trifecta.Delta (Delta(..))
 import PIC.Series.P10
 import PIC.Types
 import Control.Applicative
+import Data.Char (toUpper)
+import Data.Maybe
+import Data.List
+import Debug.Trace
 
-parsetest = parseString parser (Columns 0 0)
+parsetest p = parseString p (Columns 0 0)
 
 -- parser :: Parser (PIC, String)
 parser = (,) <$> (string "{-#" *> getDeviceP <* string "#-}") <*> parseExpr
@@ -24,9 +28,42 @@ parseExpr = (,,,) <$> funcP <*> variablesP <*> optionP <*> exprP
 
 funcP = many alphaNum
 
-variablesP = spaces *> (try (string "=" >> return [])
-    <|> ( (:) <$> many alphaNum <*> variablesP))
+variablesP = spaces *> (
+    try (string "=" >> return [])
+    <|> ( (:) <$> many alphaNum <*> variablesP)
+    )
 
 optionP = spaces *> char '|' *> many alphaNum <* char '|' <* spaces
 
 exprP = many alphaNum
+
+data Unit = Mega | Kilo | NoUnit deriving (Eq, Show)
+
+numP = do
+    binNumP
+    <|> hexNumP
+    <|> ( calcUnit <$> many digit <*> numUnitP)
+
+toDecimal :: (Char -> Int -> Int) -> [Char] -> Int
+toDecimal f ns = sum . zipWith f ns $ reverse [0 .. length ns-1]
+
+binNumP :: Parser Int
+binNumP = bin2dec <$> (string "0b" *> many digit) where
+    bin2dec nums = toDecimal bin2dec' nums
+    bin2dec' '1' x = 2^x
+    bin2dec' '0' x = 0
+    bin2dec' _ x = trace ( "the value is expected binary value composed of '0' or '1'. The written value is incorrect") undefined
+
+hexNumP = hex2dec <$> (string "0x" *> many alphaNum) where
+    hex2dec nums = toDecimal hex2dec' nums
+    hex2dec' c x = case lookup (toUpper c) $ zip "0123456789ABCDEF" [0..] of
+        Just v  -> v * 16^x
+        Nothing -> trace ( "the value is expected binary value composed of {'0' to 'F'}. The written value is incorrect") undefined
+
+numUnitP = (try $ char 'M' *> many alphaNum >> return Mega)
+    <|> (try $ char 'k' *> many alphaNum >> return Kilo)
+    <|> return NoUnit
+
+calcUnit v Mega = 10^6* read v
+calcUnit v Kilo = 10^3* read v
+calcUnit v NoUnit = read v
